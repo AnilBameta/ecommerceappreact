9722c356202ab5df0e789494a8f59fcceb158f3d
AnilBameta <anilbameta@gmail.com>
2022-02-07 21:44:06 +0530

    Backend Completed

--- ecommerceapi/index.js
+++ ecommerceapi/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 const dotenv = require('dotenv');
 
@@ -11,11 +15,18 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 app.use(express.json())
+app.use(cors());
+
+
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("Database connected"))
 .catch(err => console.log(err))
 
-app.use('/api',userRoute)
-app.use('/api',authRoute)
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000,() => console.log("Backend server running"))
